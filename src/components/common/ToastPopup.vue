8970d5ee70ce00aebb64affef3da4e5e7c9115fa
grace7937 <eunchong.k153@gmail.com>
2021-09-14 23:31:22 +0900

    Add ToastPopup css

--- src/components/common/ToastPopup.vue
+++ src/components/common/ToastPopup.vue
@@ -40,3 +40,26 @@ export default {
 	},
 };
 </script>
+
+<style scoped>
+.toast {
+	position: fixed;
+	width: 400px;
+	height: 56px;
+	background-color: #22252e;
+	border-radius: 2px;
+	box-shadow: 0 8px 20px 0 rgba(0, 0, 0, 0.2);
+	color: white;
+	bottom: -120px;
+	margin-left: -200px;
+	left: 50%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	transition: transform 750ms ease-out;
+}
+.toast.show {
+	transform: translateY(-150px);
+	transition: transform 500ms ease-in-out;
+}
+</style>
